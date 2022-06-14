2d17b43dbcd5ad9266973fddf901428c09a04b54
bryankasodu <bkasodu@gmail.com>
2022-06-14 12:04:13 +0700

    commit docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Account, AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getAccount(): Account[] {
+    return this.appService.getAccount();
+  }
 
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Account {
+  account_id: number;
+  account_name: string;
+}
+
 @Injectable()
 export class AppService {
+  private account: Account[] = [
+    { account_id: 1, account_name: 'Bryan'},
+    { account_id: 2, account_name: 'Nicholas'},
+  ];
+
+  getAccount(): Account[] {
+    return this.account;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
